85259f60918ffa37cbba80b425c5ccb3181cc84a
Frances Chong <fchongg@gmail.com>
2019-02-09 17:42:51 -0800

    Initial commit

---
+++ app/src/main/java/com/example/achieveify/MainActivity.java
@@ -0,0 +1,13 @@
+git remote add origin git@github.com:fchongg/Achieveify.gitpackage com.example.achieveify;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
